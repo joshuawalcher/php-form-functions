dce72a8bc422aed1542f2bbdc86dbe3bb3ff5497
Joshua Walcher <joshuawalcher@yahoo.com>
2016-12-12 16:46:09 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function BuildSelectBox($name,$keysAndValuesArray,$selectedValue="")
 	//Values are what the option's value is set to. Keys are what the option displays in the box - its label
 	$html = "<select class='form-control' id='$name' name='$name'>";
 	foreach ($keysAndValuesArray as $key=>$value) {
-		if ($value == $selected) {
+		if ($value == $selectedValue) {
 			$html .= "<option value='$value' selected>$key</option>";
 		} else {
 			$html .= "<option value='$value'>$key</option>";
